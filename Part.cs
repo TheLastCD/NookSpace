21cc1bb607be3545a3c62451b24c8c6d569baad1
Charlie N A <lambopuggy@gmail.com>
2020-03-30 16:32:17 +0100

    stuuff

--- Part.cs
+++ Part.cs
@@ -8,6 +8,31 @@ namespace NookSpace
 {
     class Part
     {
+        string PartID, PartNumber, Name, Description, Supplier;
+        int AmountInStock;
+        float PurchasePrice, MarkupPercentage,RRP, Tax, VAT = 20.00F;
+
+        Part(string PID, string PNum, string PNam, string PDesc,string PSup, int Stock, float PPrice, float PMark,float RP, float T)
+        {
+            PartID = PID;
+            PartNumber = PNum;
+            Name = PNam;
+            Description = PDesc;
+            Supplier = PSup;
+            AmountInStock = Stock;
+            PurchasePrice = PPrice;
+            MarkupPercentage = PMark;
+            RRP = RP;
+            if (T == -1)
+                Tax = VAT;
+            else
+                Tax = T;
+        }
+        public void Increment_Stock(int NewStock)
+        {
+            AmountInStock = NewStock;
+        }
+
 
     }
 }
